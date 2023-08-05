81002abedc582d5804af92da5ca380e76248b021
Lance Johnson <lancejjohnson@gmail.com>
2023-08-05 17:10:34 -0400

    Validate time zone

--- lib/recordid/accounts/preferences.ex
+++ lib/recordid/accounts/preferences.ex
@@ -17,22 +17,15 @@ defmodule Recordid.Accounts.Preferences do
   def changeset(preferences, attrs) do
     preferences
     |> cast(attrs, @cast)
-    |> validate_required(@required)
+    |> validate_required(@require)
     |> validate_time_zone()
   end
 
-  defp validate_time_zone(cset) do
-    # Tzdata.zone_exists?(time_zone)
-    # case get_field(cset, :time_zone) do
-    # end
-    # with {:ok, _times} <- get_changing_time_field_keys(cset),
-    #      time_zone when is_binary(time_zone) <- get_field(cset, :time_zone),
-    #      true <- Tzdata.zone_exists?(time_zone) do
-    #   cset
-    # else
-    #   {:no_changing_times, _} -> cset
-    #   nil -> add_error(cset, :time_zone, "can't be blank")
-    #   false -> add_error(cset, :time_zone, "invalid time_zone")
-    # end
+  defp validate_time_zone(%{changes: %{time_zone: time_zone}} = cset) do
+    if Tzdata.zone_exists?(time_zone) do
+      cset
+    else
+      add_error(cset, :time_zone, "invalid time zone")
+    end
   end
 end
